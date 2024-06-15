1387bf9ab9ac2d9327d839a73b7e07f0041627a0
nandu <nandu.99.nair@gmail.com>
2024-06-15 21:34:08 +0200

    [IP] Subject - Updated the code to get the image into base 64 in response

--- colorizers/colorizer.py
+++ colorizers/colorizer.py
@@ -1,6 +1,7 @@
 from colorizers import *
 import matplotlib.pyplot as plt
-
+from os import listdir
+import io
 
 class Colorizer:
     def __init__(self, greyscale_image):
@@ -27,6 +28,9 @@ class Colorizer:
         out_img_eccv16 = postprocess_tens(tens_l_orig, colorizer_eccv16(tens_l_rs).cpu())
         out_img_siggraph17 = postprocess_tens(tens_l_orig, colorizer_siggraph17(tens_l_rs).cpu())
 
+        # Convert the processed tensor to a PIL image and generate base64 String
+        color_image = self.generateBase64String(out_img_eccv16)
+
         # plt.imsave('%s_eccv16.png' % "new_eccv", out_img_eccv16)
         # plt.imsave('%s_siggraph17.png' % "new_siggraph", out_img_siggraph17)
 
@@ -51,3 +55,24 @@ class Colorizer:
         plt.title('Output (SIGGRAPH 17)')
         plt.axis('off')
         plt.show()
+
+        return color_image
+
+    def train_model(self):
+        progress = 0
+
+        for file in listdir('image_store/train'):
+            img_path = 'image_store/train' + file
+            img = load_img(img_path)
+
+    def generateBase64String(self,img_rgb):
+        # Convert to PIL Image
+        img_pil = Image.fromarray((img_rgb * 255).astype(np.uint8))
+
+        # Save image to a bytes buffer
+        buffer = io.BytesIO()
+        img_pil.save(buffer, format="PNG")
+        img_str = base64.b64encode(buffer.getvalue()).decode()
+
+        return img_str
+
--- routers/transformer.py
+++ routers/transformer.py
@@ -14,11 +14,11 @@ router = APIRouter(
 @router.post("/upload", status_code=200, response_model=ResponseDTO)
 async def upload_file(image: ImageDTO) -> ResponseDTO:
     c1 = Colorizer(image.greyscale_image)
-    c1.colorize()
+    color_image=c1.colorize()
     # You can process the image further here
     responseDTO = ResponseDTO(
         message="Image parsed successfully",
-        processed_image=image.greyscale_image,
+        processed_image=color_image,
         status_code=200
     )
     return responseDTO
